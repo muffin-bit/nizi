47700c17857d2a13ebd6f73f31c503d342d0f013
muffin-bit <38830379+muffin-bit@users.noreply.github.com>
2020-05-09 18:11:44 -0700

    Updating data, reformatting eliminations

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 
 // Hazel Chan vs Chen
 
+//
+//   CONSTANTS
+//
+
 var OFFSET = 23;
 var NORMAL_WIDTH = 6;
 var SELECT_WIDTH = 8;
@@ -10,17 +14,17 @@ var SELECT_OPACITY = 1;
 var CHART_WIDTH = 500;
 
 // Cutoffs
-var EP10_CUTOFF = 60;
-var EP16_CUTOFF = 35;
-var EP_UNKNOWN_CUTOFF = 20;
-var FINAL_CUTOFF = 9;
+// var EP10_CUTOFF = 60;
+// var EP16_CUTOFF = 35;
+// var EP_UNKNOWN_CUTOFF = 20;
+var FINAL_CUTOFF = 35;
 
-var height = 390;
+var height = 500; // Make sure to update this in css too?
 var padding = 40;
 var middlePadding = (padding * 2) + 100;
 var width = $(window).width() - middlePadding - CHART_WIDTH - 30;
 
-var episodes = ["2", "4", "6", "7 (live votes)", "9 + 10", "12", "13 (live votes)", "16"];
+var episodes = ["2", "4", "6", "9 + 10", "12", "16"];
 // var episodes = ["2", "4"];
 var totalData;
 var dFirst;
@@ -33,15 +37,27 @@ var colors = {
     "F": "gray"
 };
 
-// Set up plot
+
+
+
+
+
+//
+//   SET UP CHART
+//
+
 var svg = d3.select("#plot").append("svg")
     .attr("class", "axis")
     .attr("height", height + padding * 2)
     .attr("width", width + padding * 2);
 
 var scaleX = d3.scaleLinear().domain([0, episodes.length - 1]).range([0, width]);
-var scaleY = d3.scaleLinear().domain([0, 99]).range([0, height]);
+var scaleY = d3.scaleLinear().domain([0, 108]).range([0, height]);
 var plot = svg.append("g").attr("transform", "translate(" + padding + "," + padding + ")");
+var pathGenerator = d3.line()
+    .x(function (d) { return scaleX(d.x); })
+    .y(function (d) { return scaleY(d.rank); });
+
 
 setXAxis();
 
@@ -53,11 +69,6 @@ d3.csv("youthwithyous2.csv", parseLine, function (err, data) {
     showChart("latestRank", true);
 });
 
-// Path generator
-var pathGenerator = d3.line()
-    .x(function (d) { return scaleX(d.x); })
-    .y(function (d) { return scaleY(d.rank); });
-
 // Set notes
 for (var i = 0; i < episodes.length; i++) {
     $("#note" + i).css("left", scaleX(i) + OFFSET + 40).hide();
@@ -65,6 +76,19 @@ for (var i = 0; i < episodes.length; i++) {
 
 resetLines();
 
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  });
+};
+
+
+
+
+
+//
+//   CHART CALCULATIONS
+//
 
 function setXAxis() {
     episodes.forEach(function (episode, i) {
@@ -83,41 +107,6 @@ function setXAxis() {
     });
 }
 
-// Add rank info to data
-function processData(data) {
-    data.forEach(function(d) {
-        d.latestRank = getLatestRank(d);
-        d.currentRank = getCurrentRank(d);
-        d.isEliminated = isEliminated(d);
-        d.rankChange = getRankChange(d);
-    })
-    return data;
-}
-
-function isEliminated(d) {
-    return (d.ranking == undefined || d.latestRank > FINAL_CUTOFF);
-}
-
-// Sorts an array of objects by key
-function sortByKey(array, key, asc) {
-    return array.sort(function(a, b) {
-        var x = a[key];
-        var y = b[key];
-        if (asc) {
-            if (x == "-") {
-                return 1;
-            }
-            if (y == "-") {
-                return -1;
-            }
-            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-        }
-        if (y == "-") {
-            return -1;
-        }
-        return ((x < y) ? 1 : ((x > y) ? -1 : 0));
-    });
-}
 
 // Sort chart by key
 function toggleSort(key) {
@@ -149,12 +138,12 @@ function showChart(key, asc) {
             }
         })
         .html(function(d) {
-            var letter = '<div class="letter" style="background: ' + getBackground(d) + '; color: ' + getTextColor(d) + '">' + d.letter + '</div>';
+            var letter = '<div class="letter" style="background: ' + getLetterGradeBackground(d) + '; color: ' + getLetterGradeTextColor(d) + '">' + d.letter + '</div>';
             var rank = d.latestRank;
             if (rank == 1000) {
                 rank = "-";
             }
-            return td(rank, "smWidth") + td(d.name, "nameWidth") + td(d.company, "companyWidth") + td(letter, "smWidth") + td(displayRankChange(d), "rankWidth");
+            return td(rank, "smWidth") + td(d.name, "nameWidth") + td(d.companyEnglish, "companyWidth") + td(letter, "smWidth") + td(displayRankChange(d), "rankWidth");
         })
         .on("mouseover", function(d) {
             selectLine(d, "#line" + d.latestRank);
@@ -165,18 +154,183 @@ function showChart(key, asc) {
      return "<td class='" + cl + "'>" + str + "</td>";
  }
 
+ function resetLines() {
+     plot.selectAll("path.ranking")
+         .style("opacity", NORMAL_OPACITY)
+         .style("stroke-width", NORMAL_WIDTH);
+ }
 
+ // Select line indicated by lineId
+ function selectLine(d, lineId) {
+     // Hide other lines
+     plot.selectAll("path.ranking").style("opacity", NORMAL_OPACITY);
+
+     // Move line to front and select
+     var line = d3.select(lineId);
+     line.moveToFront();
+     line.style("opacity", SELECT_OPACITY).style("stroke-width", SELECT_WIDTH);
+
+     // Show notes
+     updateNotes(d);
+
+     // Update box
+     displayProfile(d);
+     $("#profile").show();
+     // $("#profile").css("top", getInfoTop(d));
+ }
+
+ function plotData(data) {
+     // Update y axis
+     scaleY.domain([1, getLowestRank(data)]);
+
+     var paths = plot.selectAll("path.ranking").data(data);
+     var pathGenerator = d3.line()
+     .x(function (d) { return scaleX(d.x); })
+     .y(function (d) { return scaleY(d.rank); });
+
+     paths.enter().append("path")
+         .attr("class", "ranking")
+         .attr("id", function(d) {
+             if (d.latestRank == 1) {
+                 dFirst = d;
+             }
+             if (d.specialNote != "") { // Special Line
+                 return "sline" + d.latestRank;
+             }
+             return "line" + d.latestRank;
+         })
+         .attr("d", function(d, i) {
+             return pathGenerator(d.ranking);
+         })
+         .style("stroke", function(d, i) {
+             return getLetterGradeBackground(d);
+         })
+         .style("stroke-width", NORMAL_WIDTH)
+         .on("mouseover", function (d) {
+             selectLine(d, this);
+         })
+         .on("mouseout", function(d) {
+             resetLines();
+
+             // Hide notes and box
+             $(".note").hide();
+             $("#profile").hide();
+         });
+
+     paths.exit().remove();
+ }
+
+
+
+
+
+
+//
+//   DATA PROCESSING
+//
+
+// Add rank info to data
+function processData(data) {
+    data.forEach(function(d) {
+        d.latestRank = getLatestRank(d);
+        d.currentRank = getCurrentRank(d);
+        d.isEliminated = isEliminated(d);
+        d.rankChange = getRankChange(d);
+    })
+    return data;
+}
+
+// Parse line of csv to return a new row with episode, x, rank, and rankings[]
+function parseLine(row) {
+    var r = {};
+    r.name = row["Name on Show"];
+    r.nameChinese = row["Chinese Name"];
+    r.nameFullProfile = row["Full Profile Name"];
+    r.company = row["Company"];
+    r.companyEnglish = row["Company English"];
+    r.companyChinese = row["Company Chinese"];
+    r.letter = row["Judges Evaluation 3"];
+    r.specialNote = row.note;
+    r.ranking = [];
+
+    episodes.forEach(function(episode, i) {
+        var rank = getRank(row["Ranking Ep " + episode]);
+        if (rank > 0) {
+            var o = {};
+            o.episode = episode;
+            o.x = i;
+            o.rank = rank;
+            r.ranking.push(o);
+        }
+    })
+    return r;
+
+
+}
+
+// Returns rank with image according to [change], which must be a number
+function displayRankChange(d) {
+    if (d.rankChange == "-") {
+        return "-";
+    } else if (d.rankChange > 0) {
+        return "<img src='img/up-arrow.png' class='arrow'><span class='change up'>" + d.rankChange + '<span>';
+    } else if (d.rankChange < 0) {
+        return "<img src='img/down-arrow.png' class='arrow'><span class='change down'>" + Math.abs(d.rankChange) + '<span>';
+    } else {
+        return "<img src='img/neutral-arrow.png' class='arrow'><span class='change'>0</span>";
+    }
+}
+
+function updateNotes(d) {
+    $(".note").show();
+    for (var i = 0; i < episodes.length; i++) {
+        // No ranking, contestant dropped at this point -- hide note
+        if (d.ranking[i] == undefined || d.ranking[i].rank == undefined) {
+            $("#note" + i).hide();
+        } else { // Show rank
+            var rank = d.ranking[i].rank;
+            $("#note" + i)
+                .text(rank)
+                .css("top", scaleY(rank) + OFFSET)
+                .css("background", getLetterGradeBackground(d))
+                .css("color", getLetterGradeTextColor(d));
+        }
+    }
+}
 
 // Displays profile
 function displayProfile(d) {
     $("#pic").attr("src", getImageSource(d));
     $("#infoName").text(d.name);
+    $("#infoFullName").text(getFullNameInfo(d));
     $("#infoLetter")
         .text(d.letter)
-        .css("background", getBackground(d))
-        .css("color", getTextColor(d));
-    $("#infoCompany").text(d.company);
+        .css("background", getLetterGradeBackground(d))
+        .css("color", getLetterGradeTextColor(d));
+    $("#infoCompany").text(getFullCompanyInfo(d));
     $("#infoRank").html(getRankInfo(d));
+}
+
+
+
+//
+//   HELPERS
+//
+
+function getFullNameInfo(d) {
+  var fullName = d.nameFullProfile;
+  if (d.nameChinese) {
+    fullName = fullName + " (" + d.nameChinese + ")";
+  }
+  return fullName;
+}
+
+function getFullCompanyInfo(d) {
+  var companyName = d.companyEnglish;
+  if (d.companyChinese) {
+    companyName = companyName + " (" + d.companyChinese + ")";
+  }
+  return companyName;
 }
 
 function getImageSource(d) {
@@ -187,42 +341,6 @@ function getImageSource(d) {
       return "pics/" + res[0] + "-" + res[1] + ".jpg"
     }
 }
-
-function getBackground(d) {
-    return colors[d.letter];
-}
-
-function resetLines() {
-    plot.selectAll("path.ranking")
-        .style("opacity", NORMAL_OPACITY)
-        .style("stroke-width", NORMAL_WIDTH);
-}
-
-d3.selection.prototype.moveToFront = function() {
-  return this.each(function(){
-    this.parentNode.appendChild(this);
-  });
-};
-
-// Select line indicated by lineId
-function selectLine(d, lineId) {
-    // Hide other lines
-    plot.selectAll("path.ranking").style("opacity", NORMAL_OPACITY);
-
-    // Move line to front and select
-    var line = d3.select(lineId);
-    line.moveToFront();
-    line.style("opacity", SELECT_OPACITY).style("stroke-width", SELECT_WIDTH);
-
-    // Show notes
-    updateNotes(d);
-
-    // Update box
-    displayProfile(d);
-    $("#profile").show();
-    // $("#profile").css("top", getInfoTop(d));
-}
-
 
 function getLowestRank(data) {
     var min = 100;
@@ -236,55 +354,10 @@ function getLowestRank(data) {
     return min;
 }
 
-function plotData(data) {
-
-    // Update y axis
-    scaleY.domain([1, getLowestRank(data)]);
-
-    var paths = plot.selectAll("path.ranking").data(data);
-
-
-
-    var pathGenerator = d3.line()
-    .x(function (d) { return scaleX(d.x); })
-    .y(function (d) { return scaleY(d.rank); });
-
-    paths.enter().append("path")
-        .attr("class", "ranking")
-        .attr("id", function(d) {
-            if (d.latestRank == 1) {
-                dFirst = d;
-            }
-            if (d.specialNote != "") { // Special Line
-                return "sline" + d.latestRank;
-            }
-            return "line" + d.latestRank;
-        })
-        .attr("d", function(d, i) {
-            return pathGenerator(d.ranking);
-        })
-        .style("stroke", function(d, i) {
-            return getBackground(d);
-        })
-        .style("stroke-width", NORMAL_WIDTH)
-        .on("mouseover", function (d) {
-            selectLine(d, this);
-        })
-        .on("mouseout", function(d) {
-            resetLines();
-
-            // Hide notes and box
-            $(".note").hide();
-            $("#profile").hide();
-        });
-
-    paths.exit().remove();
-}
-
 // Returns the latest rank for every contestant, 1000 for those never ranked
 function getLatestRank(d) {
     var ranking = d.ranking[d.ranking.length - 1];
-    if (ranking == undefined || ranking == "Eliminated" || ranking == "Left the show") {
+    if (ranking == undefined) {
         return 1000;
     }
     return ranking.rank;
@@ -307,22 +380,21 @@ function getRankChange(d) {
     return prevRank - getCurrentRank(d);
 }
 
-// Returns rank with image according to [change], which must be a number
-function displayRankChange(d) {
-    if (d.rankChange == "-") {
-        return "-";
-    } else if (d.rankChange > 0) {
-        return "<img src='img/up-arrow.png' class='arrow'><span class='change up'>" + d.rankChange + '<span>';
-    } else if (d.rankChange < 0) {
-        return "<img src='img/down-arrow.png' class='arrow'><span class='change down'>" + Math.abs(d.rankChange) + '<span>';
-    } else {
-        return "<img src='img/neutral-arrow.png' class='arrow'><span class='change'>0</span>";
+// Return rank or -1 if no rank (eliminated)
+function getRank(n) {
+    if (n == "-" || n == "") {
+        return -1;
     }
+    return Number(n);
+}
+
+function isEliminated(d) {
+    return (d.ranking == undefined || d.latestRank > FINAL_CUTOFF);
 }
 
 // Returns the change for current contestants, or shows elimination
 function getRankInfo(d) {
-    if (d.specialNote != "") {
+    if (d.specialNote != undefined && d.specialNote != "") {
         return d.specialNote;
     }
     if (d.ranking.length == 0) {
@@ -331,67 +403,49 @@ function getRankInfo(d) {
     if (d.isEliminated) {
         return "Eliminated in Episode " + episodes[d.ranking.length - 1];
     }
-    return "Final Member, Rank " + d.currentRank + " " + displayRankChange(d);
-}
-
-function updateNotes(d) {
-    $(".note").show();
-    for (var i = 0; i < episodes.length; i++) {
-        // No ranking, contestant dropped at this point -- hide note
-        if (d.ranking[i] == undefined) {
-            $("#note" + i).hide();
-        } else { // Show rank
-            var rank = d.ranking[i].rank;
-            $("#note" + i)
-                .text(rank)
-                .css("top", scaleY(rank) + OFFSET)
-                .css("background", getBackground(d))
-                .css("color", getTextColor(d));
-        }
-    }
+    // return "Final Member, Rank " + d.currentRank + " " + displayRankChange(d);
+    return ""
 }
 
 // Get color of note text (all white except for yellow rank C)
-function getTextColor(d) {
+function getLetterGradeTextColor(d) {
     if (d.letter == "C") {
         return "black";
     }
     return "white";
 }
 
-// Return rank or -1 if no rank (eliminated)
-function getRank(n) {
-    if (n == "-" || n == "Eliminated") {
-        return -1;
-    } else if (n == "Left the show") {
-      return -10;
-    }
-    return Number(n);
+function getLetterGradeBackground(d) {
+    return colors[d.letter];
 }
 
-// Parse line of csv to return a new row with episode, x, rank, and rankings[]
-function parseLine(row) {
-    var r = {};
-    r.name = row["Name on Show"];
-    r.nameChinese = row["Chinese Name"];
-    r.nameFullProfile = row["Full Profile Name"];
-    r.company = row["Company English Only"];
-    r.companyEnglish = row["Company English Only"];
-    r.companyChinese = row["Company Chinese Only"];
-    r.letter = row["Judges Evaluation 3"];
-    r.specialNote = row.note;
-    r.ranking = [];
-    episodes.forEach(function(episode, i) {
-        var rank = getRank(row["Ranking Ep " + episode]);
-        if (rank > 0) {
-            var o = {};
-            o.episode = episode;
-            o.x = i;
-            o.rank = rank;
-            r.ranking.push(o);
+
+
+
+
+//
+//   GENERIC HELPERS
+//
+
+// Sorts an array of objects by key
+function sortByKey(array, key, asc) {
+    return array.sort(function(a, b) {
+        var x = a[key];
+        var y = b[key];
+        if (asc) {
+            if (x == "-") {
+                return 1;
+            }
+            if (y == "-") {
+                return -1;
+            }
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
         }
-    })
-    return r;
+        if (y == "-") {
+            return -1;
+        }
+        return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+    });
 }
 
 function translate(x, y) {
